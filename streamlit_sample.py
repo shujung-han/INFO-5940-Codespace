64abdee0ca5730a3a2ac0d4dda791ad4008fc311
Shu-Jung Han <sh2329@cornell.edu>
2025-09-26 19:37:38 -0400

    update streamlit

--- streamlit_sample.py
+++ streamlit_sample.py
@@ -1,13 +1,34 @@
 import streamlit as st
+from openai import OpenAI
 
-st.set_page_config(page_title="Hello Codespaces", layout="centered")
+client = OpenAI()
 
-st.title("👋 Hello from Codespaces!")
-st.write("If you can see this page, Streamlit is running correctly inside your Codespace.")
+#Read knowledge base from data/important_knowledge.txt
+with open("data/important_knowledge.txt", "r") as f:
+	knowledge_base = f.read()
 
-name = st.text_input("What is your name?")
-if name:
-    st.success(f"Nice to meet you, {name}!")
+st.set_page_config(page_title="Hello INFO-5940", layout="centered")
+st.title("👋 Hello from INFO-5940!")
 
-st.markdown("---")
-st.caption("This is a test app for INFO 5940 Fall 2025.")
+if "messages" not in st.session_state:
+	st.session_state ["messages"] = [{"role": "system", "content": "You are a helpful assistant"}] 
+	st.session_state ["messages"] = [{"role": "user", "content": "I want you to answer questions based on the knowledge base" + knowledge_base}] 
+	st.session_state ["messages"] = [{"role": "assistant", "content": "Howdy"}] 
+	
+for msg in st.session_state.messages: 
+	if msg ["role"] != "system" and msg ["content"] != "I want you to answer questions based on the knowledge base" + knowledge_base:
+		 st.chat_message(msg["role"]).write(msg["content"]) 
+  
+if prompt := st.chat_input():
+	st.session_state.messages.append({"role": "user", "content": prompt})
+	st.chat_message("user").write(prompt)
+    
+	with st.chat_message("assistant"):
+		stream = client.chat.completions.create(
+			model="openai.gpt-4o",
+			messages=st.session_state.messages, 
+			stream=True
+		) 
+		response = st.write_stream(stream)
+		
+	st.session_state.messages.append({"role": "assistant", "content": response})
